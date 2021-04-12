d6c4021b6dc076768eed011fba1a75792fb4ce0f
Heikki Turja <hirvo@yahoo.com>
2021-04-12 17:50:43 +0300

    4.4

--- Harjoitukset/Osa4/blogi/utils/list_helper.js
+++ Harjoitukset/Osa4/blogi/utils/list_helper.js
@@ -1,8 +1,13 @@
 const dummy = (blogs) => {
-    const test = 1
-    return test
-  }
-  
-  module.exports = {
-    dummy
-  }
+  const test = 1
+  return test
+}
+
+const totalLikes = (blogs) => {
+
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+module.exports = { dummy, totalLikes }
