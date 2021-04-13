4ead8bbfc071ecd334f7fb6f74d0221ea2ec9335
Heikki Turja <hirvo@yahoo.com>
2021-04-13 20:05:30 +0300

    4.13 done and tested

--- Harjoitukset/Osa4/blogi/tests/blog_api.test.js
+++ Harjoitukset/Osa4/blogi/tests/blog_api.test.js
@@ -3,11 +3,11 @@ const supertest = require('supertest')
 const helper = require('./test_helper')
 const app = require('../app')
 const api = supertest(app)
-const Note = require('../models/blog')
+const Blog = require('../models/blog')
 
 beforeEach(async () => {
-  await Note.deleteMany({})
-  await Note.insertMany(helper.initialBlogs)
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
 })
 
 test('blogs are returned as json', async () => {
@@ -23,7 +23,7 @@ test('all blogs are returned', async () => {
   expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
 
-test('a specific blog is within the returned notes', async () => {
+test('a specific blog is within the returned blogs', async () => {
   const response = await api.get('/api/blogs')
 
   const titles = response.body.map(r => r.title)
@@ -113,7 +113,56 @@ test('a blog without a url is not added ', async () => {
   expect(blogsAfterAdd).toHaveLength(helper.initialBlogs.length)
 })
 
+describe('deletion of a blog', () => {
+  test('succeeds with status code 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDB()
+    const blogToDelete = blogsAtStart[0]
 
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
+
+    const blogAtEnd = await helper.blogsInDB()
+
+    expect(blogAtEnd).toHaveLength(
+      helper.initialBlogs.length - 1
+    )
+
+    const titles = blogAtEnd.map(r => r.title)
+
+    expect(titles).not.toContain(blogToDelete.title)
+  })
+
+  test('succeeds with status code 400 if id is not valid', async () => {
+    await api
+      .delete('/api/blogs/66')
+      .expect(400)
+
+    const blogAtEnd = await helper.blogsInDB()
+
+    expect(blogAtEnd).toHaveLength(
+      helper.initialBlogs.length
+    )
+  })
+})
+
+describe('updating of a blog', () => {
+  test('succeeds with valid data', async () => {
+    const blogsAtStart = await helper.blogsInDB()
+    const blogToUpdate = blogsAtStart[0]
+
+    const newBlog = { ...blogToUpdate, likes: 6 }
+
+    await api
+      .put(`/api/blogs/${blogToUpdate.id}`)
+      .send(newBlog)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDB()
+    expect(blogsAtEnd[0].likes).toBe(6)
+  })
+})
 
 afterAll(() => {
   mongoose.connection.close()
